07d814d53cb1ef27aa7ca9adba82438418ccf202
Yashika bokre <83172795+Yashikabokre@users.noreply.github.com>
2022-09-22 12:22:20 +0530

    Add files via upload
    completed the first server

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const http = require('http');
+const http = require("http"); // package import 
 
-const port = 8081;
+const port = 8081; //local port number
 
 http
     .createServer((request, response) => {
+        //call back function
 	    response.writeHead(200,{"Content-Type" : "text/html"});
         response.write("<h1>Hello, this is from my server</h1>");
     	response.end();
     })
 
     .listen(port, () => {
-	    console.log(`Nodejs server on port $(port)`);
+	    console.log(`Nodejs server on port ${port}`);
     });
+
+    // http:localhost:8081
