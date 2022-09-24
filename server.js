7976aee924a82422f2cfb45382df1ab7f57f9936
Yashika bokre <83172795+Yashikabokre@users.noreply.github.com>
2022-09-25 00:47:06 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,12 +2,37 @@ const http = require("http"); // package import
 
 const port = 8081; //local port number
 
+const todolist = ["Complete Node Bytes", "Play Cricket"]
+
 http
     .createServer((request, response) => {
-        //call back function
-	    response.writeHead(200,{"Content-Type" : "text/html"});
-        response.write("<h1>Hello, this is from my server</h1>");
-    	response.end();
+        const {method, url } = request;
+
+        if (url == "/todo") {
+            if(method == "GET") {
+                response.writeHead(200, {'Content-Type': "text/html"});
+                response.write(todolist.toString());
+            } else if( method === "POST"){
+                let body = "";
+                request.on("error",(err) => {
+                    console.error(err);
+                })
+                .on("data",(chunk) => {
+                    body += chunk;
+                    console.log(chunk);
+                })
+                .on("end", () =>{
+                    body = JSON.parse(body);
+                    console.log("data:", body);
+                });
+            } else{
+                response.writeHead(501);
+            }
+        } else{
+            response.writeHead(404);
+        }
+
+        response.end();
     })
 
     .listen(port, () => {
